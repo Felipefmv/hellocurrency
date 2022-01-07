7b6bc9faef1e056235c6329f5e69a9c94b33bed4
Felipe Fernandes <felipefernandesmiranda@gmail.com>
2022-01-07 09:09:20 -0300

    :zap: readme atualizado

--- src/components/Dashboard/style.js
+++ src/components/Dashboard/style.js
@@ -4,9 +4,10 @@ export const Container = styled.div`
   display: grid;
   grid-template-columns: 20rem 20rem;
   grid-gap: 10px;
-  text-align: center;
-
+  justify-content: center;
+  margin-top: 2rem;
   input {
     margin-left: 1.5rem;
-  }
+    padding: 0.7rem;
+  
 `
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components'
 
 export const Container = styled.header`
-    
-    
+  margin: 1rem;
 `
